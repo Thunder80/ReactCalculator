3ed1949541b849d42d2c35d3ef2968543f102c8c
Sayan Bhattacharya <bhattacharya.sayan30@gmail.com>
2020-01-26 19:36:15 +0530

    minor changes

--- src/context/ScreenContextProvider.js
+++ src/context/ScreenContextProvider.js
@@ -4,82 +4,70 @@ export const ScreenContext = createContext();
 
 const ScreenContextProvider = (props) => {
 	const [screen, setScreen] = useState("+");
-	let start = false;
-	let n = 0;
 	const [sign, setSign] = useState('+');
 	const [answer, setAnswer] = useState(0);
-	const [pressSign, setPressSign] = useState(false);
+
+	const updateAnswer = (sign) => {
+		if(!isNaN(parseInt(screen.substring(1))))
+		if(sign === '+')
+				setAnswer(answer + parseInt(screen.substring(1)));
+			else if(sign === '-')
+				setAnswer(answer - parseInt(screen.substring(1)));
+			else if(sign === '*')
+				setAnswer(answer * parseInt(screen.substring(1)));
+			else if(sign === '/')
+				setAnswer(answer / parseInt(screen.substring(1)));
+			else if(sign === '%')
+				setAnswer(answer % parseInt(screen.substring(1)));
+		};
 
 	const updateScreen = (input) => {
 		if(!isNaN(parseInt(input)))
-			start = true;
-		if(!start)
 		{
-			if(input === '+')
-			{
-				setSign('+');
-				setScreen('+');
-			}
-			else if(input === '-')
-			{
-				setSign('-');	
-				setScreen('-');
-			}
-			else if(input === '*')
-			{
-				setSign('*');
-				setScreen('*');
-			}
-			else if(input === '/')
-			{
-				setSign('/');
-				setScreen('/');
-			}
-			else if(input === '%')
-			{
-				setSign('%');
-				setScreen('%');
-			}
+			setScreen(screen + input);
 		}
-		else
+		else if(input === '+')
 		{
-			if(!isNaN(parseInt(input)))
-			{
-				setScreen(screen + input);
-				setPressSign(true);
-			}
-			else if(input === '+')
-			{
-				setSign('+');
-				setScreen('+');
-			}
-			else if(input === '-')
-			{
-				setSign('-');	
-				setScreen('-');
-			}
-			else if(input === '*')
-			{
-				setSign('*');
-				setScreen('*');
-			}
-			else if(input === '/')
-			{
-				setSign('/');
-				setScreen('/');
-			}
-			else if(input === '%')
-			{
-				setSign('%');
-				setScreen('%');
-			}
+			updateAnswer(sign);
+			setSign('+');
+			setScreen('+');
+		}
+		else if(input === '-')
+		{
+			updateAnswer(sign);
+			setSign('-');	
+			setScreen('-');
+		}
+		else if(input === '*')
+		{
+			updateAnswer(sign);
+			setSign('*');
+			setScreen('*');
+		}
+		else if(input === '/')
+		{
+			updateAnswer(sign);
+			setSign('/');
+			setScreen('/');
+		}
+		else if(input === '%')
+		{
+			updateAnswer(sign);
+			setSign('%');
+			setScreen('%');
+		}
+		else if(input === '=')
+		{
+			updateAnswer(sign);
+			setSign('+');
+			setScreen('+');
 		}
 	};
 
-	return(
-		<ScreenContext.Provider value={{screen:screen, updateScreen:updateScreen, answer: answer}}>
-			{props.children}
-		</ScreenContext.Provider>
+return(
+	<ScreenContext.Provider value={{screen:screen, updateScreen:updateScreen, answer: answer}}>
+	{props.children}
+	</ScreenContext.Provider>
 	); 
 };
 
